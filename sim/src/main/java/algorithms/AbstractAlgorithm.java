4222828a142e63f93334ff3f9f398e8e166aa47d
Romain F. T <rrroschan@gmail.com>
2019-01-23 17:28:04 +0100

    interface pour abstractalgo

--- sim/src/main/java/algorithms/AbstractAlgorithm.java
+++ sim/src/main/java/algorithms/AbstractAlgorithm.java
@@ -1,4 +1,21 @@
 package algorithms;
 
 public class AbstractAlgorithm {
+	private AbstractGraph graph;
+	
+	public void initGraph() {
+		
+	}
+	
+	public void shuffle() {
+		
+	}
+	
+	public void testKhi2() {
+		
+	}
+	
+	public AbstractNode nextPeer() {
+		
+	}
 }
