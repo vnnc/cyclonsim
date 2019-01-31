4dd233ebfc57d800857e0467557b8f7b7c7de028
Romain F. T <rrroschan@gmail.com>
2019-01-31 18:31:24 +0100

    Légère amélioration de la lisibilité du code

--- sim/src/main/java/algorithms/AlgorithmBasic.java
+++ sim/src/main/java/algorithms/AlgorithmBasic.java
@@ -8,12 +8,8 @@ import models.SimpleNode;
 import java.util.*;
 
 //TODO utiliser les mêmes termes qu'eux :
-// - maxPeers (>=2) (= shuffleLength ?)
-// - round(s) (= appeler la fonction shuffle sur tous les pairs)
-// - partialView == peerNeighbors
+// - partialView == peerNeighbors ?
 // - etc.
-
-//mettre un cache (= liste de voisins) (= partialView)
 
 public class AlgorithmBasic extends AbstractAlgorithm {
 
@@ -51,102 +47,106 @@ public class AlgorithmBasic extends AbstractAlgorithm {
 		and a random peer, Q, within this subset, where l is a system parameter,
 		called shuffle length. */
 		int neighborsSubsetSize = Math.min(shuffleLength, nodeNeighbors.size());
-		if(nodeNeighbors.size() > 0) {
-			ArrayList<SimpleNode> nodeNeighborsSubset = new ArrayList<SimpleNode>();
-			nodeNeighborsSubset.addAll(nodeNeighbors);
-			Collections.shuffle(nodeNeighborsSubset);
-			System.out.println("DEBUG: nodeNeighborsSubset: " + nodeNeighborsSubset);
-			
-			nodeNeighborsSubset = new ArrayList<SimpleNode>(nodeNeighborsSubset.subList(0, neighborsSubsetSize));
-			System.out.println("Subset of neighbors: " + nodeNeighborsSubset);
+		if(nodeNeighbors.size() <= 0) {
+			System.out.println("[Error during shuffle] Graph: " + this.graph);
+			return;
+		}
+		ArrayList<SimpleNode> nodeNeighborsSubset = new ArrayList<SimpleNode>();
+		nodeNeighborsSubset.addAll(nodeNeighbors);
+		Collections.shuffle(nodeNeighborsSubset);
+		System.out.println("DEBUG: nodeNeighborsSubset: " + nodeNeighborsSubset);
+		
+		nodeNeighborsSubset = new ArrayList<SimpleNode>(nodeNeighborsSubset.subList(0, neighborsSubsetSize));
+		System.out.println("Subset of neighbors: " + nodeNeighborsSubset);
 
-			SimpleNode peer = nodeNeighborsSubset.get(rng.nextInt(nodeNeighborsSubset.size()));
-			peer = this.graph.getNodeByLabel(peer.getLabel());
+		SimpleNode peer = nodeNeighborsSubset.get(rng.nextInt(nodeNeighborsSubset.size()));
+		peer = this.graph.getNodeByLabel(peer.getLabel());
 
-			// peer added to choosenPeers for chi-squared test computations
-			this.chosenPeers.add(peer.getLabel());
-			System.out.println("Chosen peer: " + peer);
-
-			ArrayList<SimpleNode> subsetForPeer = new ArrayList<SimpleNode>();
-			for(SimpleNode n : nodeNeighborsSubset) {
-				if(n.getLabel() == peer.getLabel()) {
-					subsetForPeer.add(node);
-				} else {
-					subsetForPeer.add(n);
-				}
-			}
-			System.out.println("Subset sent to peer: " + subsetForPeer);
-			
-			ArrayList<SimpleNode> peerNeighbors = this.graph.getNeighborsOfNode(peer);
-			System.out.println("Peer neighbors: " + peerNeighbors);
-			int peerNeighborsSubsetSize = Math.min(shuffleLength, peerNeighbors.size());
-
-			if (peerNeighbors.size() > 0) {
-				ArrayList<SimpleNode> peerNeighborsSubset = new ArrayList<SimpleNode>();
-				peerNeighborsSubset.addAll(peerNeighbors);
-				Collections.shuffle(peerNeighborsSubset);
-				System.out.println("DEBUG: peerNeighborsSubset: " + peerNeighborsSubset);
-				peerNeighborsSubset = new ArrayList<SimpleNode>(peerNeighborsSubset.subList(0, peerNeighborsSubsetSize));
-				System.out.println("Subset of peer neighbors: " + peerNeighborsSubset);
-
-				/* 5. Discard entries pointing to P, and entries that are already
-				in P’s cache. */
-				ArrayList<SimpleEdge> newEdges = new ArrayList<SimpleEdge>();
-				ArrayList<SimpleEdge> removedEdges = new ArrayList<SimpleEdge>();
-				ArrayList<SimpleNode> nodeKeptEntries = new ArrayList<SimpleNode>();
-				for (SimpleNode n : peerNeighborsSubset) {
-					n = this.graph.getNodeByLabel(n.getLabel());
-					if(this.graph.getEdge(n, node) == null
-					&& !nodeNeighbors.contains(n)
-					&& n.getLabel() != nodeLabel) {
-						if (nodeNeighbors.size() > cacheSize
-						&& subsetForPeer.get(subsetForPeer.size()-1) != node) {
-							SimpleNode removeTarget = subsetForPeer.get(subsetForPeer.size()-1);
-							removeTarget = this.graph.getNodeByLabel(removeTarget.getLabel());
-							SimpleEdge toRemove = this.graph.getEdge(node, removeTarget);
-							this.graph.removeEdge(toRemove);
-							removedEdges.add(toRemove);
-						}
-						if (nodeNeighbors.size() < cacheSize
-						|| subsetForPeer.get(subsetForPeer.size()-1) != node) {
-							nodeKeptEntries.add(n);
-							this.graph.addEdge(node,n);
-							newEdges.add(this.graph.getEdge(node, n));
-							nodeNeighbors.add(n);
-						}
-					}
-				}
-
-				System.out.println("Remaining entries within subset from peer: " + nodeKeptEntries);
-				ArrayList<SimpleNode> peerKeptEntries = new ArrayList<SimpleNode>();
-
-				/* 6. Update P’s cache to include all remaining entries, by firstly
-				using empty cache slots (if any), and secondly replacing entries
-				among the ones originally sent to Q. */
-				for (SimpleNode n : subsetForPeer) {
-					n = this.graph.getNodeByLabel(n.getLabel());
-					if (this.graph.getEdge(n, peer) == null
-					&& !peerNeighbors.contains(n)
-					&& n.getLabel() != peer.getLabel()) {
-						if(peerNeighbors.size() > cacheSize) {
-							SimpleNode removeTarget = peerNeighborsSubset.get(peerNeighborsSubset.size()-1);
-							removeTarget = this.graph.getNodeByLabel(removeTarget.getLabel());
-							SimpleEdge toRemove = this.graph.getEdge(peer, removeTarget);
-							this.graph.removeEdge(toRemove);
-							removedEdges.add(toRemove);
-						}
-						peerKeptEntries.add(n);
-						this.graph.addEdge(peer, n);
-						newEdges.add(this.graph.getEdge(peer, n));
-						peerNeighbors.add(n);
-					}
-				}
-
-				System.out.println("Remaining entries within subset from node: " + peerKeptEntries);
-				System.out.println("Created Edges: " + newEdges);
-				System.out.println("Removed Edges: " + removedEdges);
+		// peer added to choosenPeers for chi-squared test computations
+		this.chosenPeers.add(peer.getLabel());
+		System.out.println("Chosen peer: " + peer);
+		
+		/* 3. Send the updated subset to Q. */
+		ArrayList<SimpleNode> subsetForPeer = new ArrayList<SimpleNode>();
+		for(SimpleNode n : nodeNeighborsSubset) {
+			if(n.getLabel() == peer.getLabel()) {
+				subsetForPeer.add(node);
+			} else {
+				subsetForPeer.add(n);
 			}
 		}
+		System.out.println("Subset sent to peer: " + subsetForPeer);
+		
+		/* 4. Receive from Q a subset of no more than l of Q’s neighbors. */
+		ArrayList<SimpleNode> peerNeighbors = this.graph.getNeighborsOfNode(peer);
+		System.out.println("Peer neighbors: " + peerNeighbors);
+		int peerNeighborsSubsetSize = Math.min(shuffleLength, peerNeighbors.size());
+		if (peerNeighbors.size() <= 0) {
+			System.out.println("[Error during shuffle] Graph: " + this.graph);
+			return;
+		}
+		ArrayList<SimpleNode> peerNeighborsSubset = new ArrayList<SimpleNode>();
+		peerNeighborsSubset.addAll(peerNeighbors);
+		Collections.shuffle(peerNeighborsSubset);
+		System.out.println("DEBUG: peerNeighborsSubset: " + peerNeighborsSubset);
+		peerNeighborsSubset = new ArrayList<SimpleNode>(peerNeighborsSubset.subList(0, peerNeighborsSubsetSize));
+		System.out.println("Subset of peer neighbors: " + peerNeighborsSubset);
+
+		/* 5. Discard entries pointing to P, and entries that are already in P’s cache. */
+		ArrayList<SimpleEdge> newEdges = new ArrayList<SimpleEdge>();
+		ArrayList<SimpleEdge> removedEdges = new ArrayList<SimpleEdge>();
+		ArrayList<SimpleNode> nodeKeptEntries = new ArrayList<SimpleNode>();
+		for (SimpleNode n : peerNeighborsSubset) {
+			n = this.graph.getNodeByLabel(n.getLabel());
+			if(this.graph.getEdge(n, node) == null
+			&& !nodeNeighbors.contains(n)
+			&& n.getLabel() != nodeLabel) {
+				if (nodeNeighbors.size() > cacheSize
+				&& subsetForPeer.get(subsetForPeer.size()-1) != node) {
+					SimpleNode removeTarget = subsetForPeer.get(subsetForPeer.size()-1);
+					removeTarget = this.graph.getNodeByLabel(removeTarget.getLabel());
+					SimpleEdge toRemove = this.graph.getEdge(node, removeTarget);
+					this.graph.removeEdge(toRemove);
+					removedEdges.add(toRemove);
+				}
+				if (nodeNeighbors.size() < cacheSize
+				|| subsetForPeer.get(subsetForPeer.size()-1) != node) {
+					nodeKeptEntries.add(n);
+					this.graph.addEdge(node, n);
+					newEdges.add(this.graph.getEdge(node, n));
+					nodeNeighbors.add(n);
+				}
+			}
+		}
+
+		System.out.println("Remaining entries within subset from peer: " + nodeKeptEntries);
+		ArrayList<SimpleNode> peerKeptEntries = new ArrayList<SimpleNode>();
+
+		/* 6. Update P’s cache to include all remaining entries, by firstly
+		using empty cache slots (if any), and secondly replacing entries
+		among the ones originally sent to Q. */
+		for (SimpleNode n : subsetForPeer) {
+			n = this.graph.getNodeByLabel(n.getLabel());
+			if (this.graph.getEdge(n, peer) == null
+			&& !peerNeighbors.contains(n)
+			&& n.getLabel() != peer.getLabel()) {
+				if(peerNeighbors.size() > cacheSize) {
+					SimpleNode removeTarget = peerNeighborsSubset.get(peerNeighborsSubset.size()-1);
+					removeTarget = this.graph.getNodeByLabel(removeTarget.getLabel());
+					SimpleEdge toRemove = this.graph.getEdge(peer, removeTarget);
+					this.graph.removeEdge(toRemove);
+					removedEdges.add(toRemove);
+				}
+				peerKeptEntries.add(n);
+				this.graph.addEdge(peer, n);
+				newEdges.add(this.graph.getEdge(peer, n));
+				peerNeighbors.add(n);
+			}
+		}
+		System.out.println("Remaining entries within subset from node: " + peerKeptEntries);
+		System.out.println("Created Edges: " + newEdges);
+		System.out.println("Removed Edges: " + removedEdges);
+		
 		System.out.println("Graph after shuffle: " + this.graph);
 	}
 
