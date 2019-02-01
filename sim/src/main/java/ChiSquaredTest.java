578d269028e1dfeb9c1a5ac722da2ca5d2350e70
Romain F. T <rrroschan@gmail.com>
2019-02-01 09:58:17 +0100

    rien d'intéressant

--- sim/src/main/java/ChiSquaredTest.java
+++ sim/src/main/java/ChiSquaredTest.java
@@ -1,5 +1,8 @@
-package algorithms;
+//la package algorithms c'est pour les nombreuses variantes de cyclon qu'on va
+//faire, mais le test du chi² est entièrement indépendant de cela et ne rentre
+//donc pas dans ce package. À la limite il peut aller dans utilities ?
 
+import algorithms.AbstractAlgorithm;
 import models.*;
 import org.apache.commons.math3.distribution.ChiSquaredDistribution;
 import org.apache.commons.math3.distribution.NormalDistribution;
@@ -17,23 +20,24 @@ public class ChiSquaredTest {
 		this.initialGraph = initialGraph;
 	}
 
-
 	/**
 	 *
-	 * @param nodeLabel label (étiquette) du noeud du graph qui est observé dans le test
+	 * @param nodeLabel label (étiquette) du noeud du graphe qui est observé dans le test
 	 * @param peerAmount nombre d'appels de nextPeer par calcul de valeur du test X²
 	 * @see AbstractAlgorithm#nextPeer(Object)
 	 * @see #runComputation(int, int, int)
-	 * @param shuffleInterval nombre de shuffle sur tout les noeuds du graph entre chaque appel de nextPeer
-	 *                        exemple: Si shuffleInterval vaut 5, entre chaque appel de nextPeer dans la méthode de calcul
-	 *                        du test X² le graph passera par 5 shuffle
-	 * @param cacheSize taille maximale de la vue partielle d'un noeud du graph
-	 * @param shuffleLength taille de la partie de la vue partielle considérée lors d'un shuffle
-	 * @param confidenceLevel niveau de confiance, exemple: si vaut 0.98 , une valeur qui suit la loi de distribution a 98% de chance
-	 *                        de se trouver dans l'intervalle de confiance
+	 * @param shuffleInterval nombre de shuffle sur tout les noeuds du graph entre
+	 *                        chaque appel de nextPeer. Exemple: Si shuffleInterval
+	 *                        vaut 5, entre chaque appel de nextPeer dans la méthode
+	 *                        de calcul du test X² le graph passera par 5 shuffle
+	 * @param cacheSize taille maximale de la vue partielle d'un noeud du graphe
+	 * @param shuffleLength taille du sous-ensemble de la vue partielle considérée lors d'un shuffle
+	 * @param confidenceLevel niveau de confiance, exemple: si vaut 0.98 , une
+	 *                        valeur qui suit la loi de distribution a 98% de
+	 *                        chance de se trouver dans l'intervalle de confiance
 	 */
-	public void runFullTest(int nodeLabel,int peerAmount,int shuffleInterval,int cacheSize,int shuffleLength,double confidenceLevel)
-	{
+	public void runFullTest(int nodeLabel, int peerAmount, int shuffleInterval,
+	                        int cacheSize, int shuffleLength, double confidenceLevel) {
 		// Liste contenant les valeurs du test X² calculées
 		ArrayList<Double> values = new ArrayList<Double>();
 
@@ -47,22 +51,20 @@ public class ChiSquaredTest {
 		double limitValue = -td.inverseCumulativeProbability(alpha);
 
 		int n = 0;
-		do{
+		do {
 			n++;
-			this.algorithm.initGraph(initialGraph,cacheSize,shuffleLength);
-			values.add(runComputation(nodeLabel,peerAmount,shuffleInterval));
+			this.algorithm.initGraph(initialGraph, cacheSize, shuffleLength);
+			values.add(runComputation(nodeLabel, peerAmount, shuffleInterval));
 			standardErrors.add(Math.sqrt(computeVariance(values)/n));
-			Utilities.printInfo("Computed standard error: "+standardErrors.get(n-1));
-		}while(standardErrors.get(n-1) > limitValue);
+			Utilities.printInfo("Computed standard error: " + standardErrors.get(n-1));
+		} while(standardErrors.get(n-1) > limitValue);
 
 		// Moyenne des valeurs du test X² calculées
 		double chiMean = computeMean(values);
 
-		Utilities.printInfo("Mean ChiSquared value: "+chiMean);
-
-		Utilities.printInfo("Maximum ChiSquared value: "+Collections.max(values));
-
-		Utilities.printInfo("Minimum ChiSquared value: "+Collections.min(values));
+		Utilities.printInfo("Mean ChiSquared value: " + chiMean);
+		Utilities.printInfo("Maximum ChiSquared value: " + Collections.max(values));
+		Utilities.printInfo("Minimum ChiSquared value: " + Collections.min(values));
 
 		// Borne supérieure de l'intervalle de confiance selon les valeurs calculées
 		double rightBound = chiMean + limitValue*standardErrors.get(n-1);
@@ -73,25 +75,24 @@ public class ChiSquaredTest {
 		// Valeur critique selon la distribution X²
 		double criticalValue = csd.inverseCumulativeProbability(confidenceLevel);
 
-		Utilities.printInfo("Expected critical value for ChiSquared distribution : "+criticalValue);
-
-		Utilities.printInfo("Computed confidence interval: "+"["+leftBound+" , "+rightBound+"]");
+		Utilities.printInfo("Expected critical value for ChiSquared distribution: " + criticalValue);
+		Utilities.printInfo("Computed confidence interval: "
+		                    + "[" + leftBound + " , " + rightBound + "]");
 	}
 
 	private double computeMean(ArrayList<Double> values){
 		double sum = 0;
-		for(Double value : values){
+		for(Double value : values) {
 			sum+=value;
 		}
 		return sum/values.size();
 	}
 
 	private double computeVariance(ArrayList<Double> values){
-
 		double sumExp = 0;
 		double sumExpSquared = 0;
 
-		for(Double value : values){
+		for(Double value : values) {
 			double valueSquared = value*value;
 			sumExp += value;
 			sumExpSquared += valueSquared;
@@ -99,20 +100,15 @@ public class ChiSquaredTest {
 
 		double exp = sumExp/values.size();
 		double expSquared = sumExpSquared/values.size();
-
 		return (expSquared - exp);
 	}
 
-	private double runComputation(int nodeLabel,int peerAmount,int shuffleInterval) {
-
+	private double runComputation(int nodeLabel, int peerAmount, int shuffleInterval) {
 		int graphSize = this.algorithm.getGraph().vertexSet().size();
-
 		double expectedFreq = 1.0/graphSize;
-
 		ArrayList<Integer> chosenPeers = new ArrayList<Integer>();
 
-		for(int i=0;i<peerAmount;i++)
-		{
+		for(int i=0; i<peerAmount; i++) {
 			this.algorithm.multiShuffleAll(shuffleInterval);
 			chosenPeers.add(this.algorithm.nextPeer(this.algorithm.getGraph().getNodeByLabel(nodeLabel)).getLabel());
 		}
@@ -129,13 +125,12 @@ public class ChiSquaredTest {
 		}
 
 		double res = 0;
-
 		for (Map.Entry<Integer, Double> entry : frequencies.entrySet()) {
-			Utilities.printDebug("(k,v) = ("+entry.getKey()+","+entry.getValue()+")");
-			res+= Math.pow((entry.getValue() - expectedFreq),2)/expectedFreq;
+			Utilities.printDebug("(k,v) = (" + entry.getKey() + "," + entry.getValue() + ")");
+			res+= Math.pow((entry.getValue() - expectedFreq), 2)/expectedFreq;
 		}
-
 		res *= chosenPeers.size();
 		return res;
 	}
 }
+
--- sim/src/main/java/Main.java
+++ sim/src/main/java/Main.java
@@ -1,29 +1,26 @@
 import algorithms.*;
 import models.*;
+//import ChiSquaredTest;
 import org.apache.commons.math3.distribution.ChiSquaredDistribution;
 
-// TODO oh non il veut qu'on fasse le graphe nous-mêmes ??
-
-// TODO faire le test plein de fois pour avoir des résultats statistiquement valables
+// TODO oh non veut-il qu'on fasse le graphe nous-mêmes ??
 
 // [divers algos] -> interface getNextPeer() -> le khi² qui fait 1000× "appeler getNextPeer() 1000×" et en calcule la moyenne
-// en fait osef de la moyenne, il faut l'intervalle de confiance : on arrête quand (la racine de la variance / nombre de fois qu'on a fait le test) est faible
-// faible = alpha qui dépend de la probabilité que ce soit juste, qu'on se fixe
+// en fait osef de la moyenne, il faut l'intervalle de confiance : on s'arrête
+// quand (la racine de la variance / nombre de fois qu'on a fait le test) est faible
+// où "faible" = alpha qui dépend de la probabilité que ce soit juste, qu'on se fixe
 
 public class Main {
-
-
 	public static void main(String args[]) {
-
 		AlgorithmBasic algo = new AlgorithmBasic();
 		Graph g = new Graph(SimpleNode.class,SimpleEdge.class);
 		g.importFromCSV("testgraph1.csv");
+		final int SHUFFLE_INTERVAL = 150;
+		final int PEER_AMOUNT = 20;
 		final int CACHE_SIZE = 3;
 		final int SHUFFLE_LENGTH = 2;
-
-		ChiSquaredTest test = new ChiSquaredTest(algo,g);
-
-		test.runFullTest(0,150,20,CACHE_SIZE,SHUFFLE_LENGTH,0.98);
-
+		final double CONFIDENCE_LEVEL = 0.98;
+		ChiSquaredTest test = new ChiSquaredTest(algo, g);
+		test.runFullTest(0, SHUFFLE_INTERVAL, PEER_AMOUNT, CACHE_SIZE, SHUFFLE_LENGTH, CONFIDENCE_LEVEL);
 	}
 }
