c3b1bf823702da1ebccb10db52786d201c3a23d2
Romain F. T <rrroschan@gmail.com>
2019-03-06 23:58:43 +0100

    Léger changement de la structure du projet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 		AlgorithmCyclonBasic algo = new AlgorithmCyclonBasic();
 		Graph g = new Graph(SimpleNode.class, SimpleEdge.class);
-		g.importFromCSV("testgraph3.csv");
+		g.importFromCSV("graphs/testgraph3.csv");
 		final int PEER_AMOUNT = 50;
 		final int SHUFFLE_INTERVAL = 1;
 		final int CACHE_SIZE = 6;
@@ -23,11 +23,11 @@ public class Main {
 		boolean reussite = test.runFullTest(0, PEER_AMOUNT, SHUFFLE_INTERVAL, CACHE_SIZE, SHUFFLE_LENGTH, CONFIDENCE_LEVEL);
 		Utilities.printInfo("Le test passe-t-il avec succès ? " + reussite);
 		Utilities.printInfo((int) (100.0*algo.nbEchanges/(algo.nbEchanges+algo.nbEchecs)) + "% de shuffling");
-		g.exportToCSV("outputgraph.csv");
+		g.exportToCSV("graphs/output.csv");
 
 //		Graph g = new Graph(SimpleNode.class,SimpleEdge.class);
 //		g.generateRandom(20,6);
-//		g.exportToCSV("sim/testgraph3.csv");
+//		g.exportToCSV("graphs/testgraph3.csv");
 //		System.out.println(g);
 
 	}
